04b206e5c278f0bfac581b3c5977a380fcb76e53
SuiYUeChang <suiyuechangyishangbo@gmail.com>
2017-07-06 14:49:46 +0800

    add Lasso at 0706

--- SklearnNote.py
+++ SklearnNote.py
@@ -41,3 +41,25 @@ reg.fit([[0, 0], [0, 0], [1, 1]], [0, .1, 1])
 reg.alpha_                                      
 #0.1
 
+##Lasso
+
+#The Lasso is a linear model that estimates sparse coefficients.
+# It is useful in some contexts due to its tendency to prefer solutions with fewer parameter values, 
+# effectively reducing the number of variables upon which the given solution is dependent. 
+# For this reason, the Lasso and its variants are fundamental to the field of compressed sensing.
+# Under certain conditions, it can recover the exact set of non-zero weights 
+# (see Compressive sensing: tomography reconstruction with L1 prior (Lasso)).
+# Mathematically, it consists of a linear model trained with \ell_1 prior as regularizer. 
+# The objective function to minimize is:
+#\underset{w}{min\,} { \frac{1}{2n_{samples}} ||X w - y||_2 ^ 2 + \alpha ||w||_1}
+#The lasso estimate thus solves the minimization of the least-squares penalty with \alpha ||w||_1 added, 
+# where \alpha is a constant and ||w||_1 is the \ell_1-norm of the parameter vector.
+
+from sklearn import linear_model
+reg = linear_model.Lasso(alpha = 0.1)
+reg.fit([[0, 0], [1, 1]], [0, 1])
+#Lasso(alpha=0.1, copy_X=True, fit_intercept=True, max_iter=1000,
+   #normalize=False, positive=False, precompute=False, random_state=None,
+   #selection='cyclic', tol=0.0001, warm_start=False)
+reg.predict([[1, 1]])
+#array([ 0.8])
